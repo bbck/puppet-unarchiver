d6050b5e0393b6f7d204f060cbac3fb77cc37dbe
Chris Boyle <chris@bbck.net>
2014-11-26 22:13:19 -0500

    Make package name exactly same as app name

--- spec/classes/unarchiver_spec.rb
+++ spec/classes/unarchiver_spec.rb
@@ -5,7 +5,7 @@ describe 'unarchiver' do
   version = '3.9.1'
 
   it { should contain_class('unarchiver') }
-  it { should contain_package("TheUnarchiver-#{version}").with_provider('compressed_app') }
-  it { should contain_package("TheUnarchiver-#{version}").with_source("https://theunarchiver.googlecode.com/files/TheUnarchiver#{version}.zip") }
+  it { should contain_package("The Unarchiver").with_provider('compressed_app') }
+  it { should contain_package("The Unarchiver").with_source("https://theunarchiver.googlecode.com/files/TheUnarchiver#{version}.zip") }
 
 end
